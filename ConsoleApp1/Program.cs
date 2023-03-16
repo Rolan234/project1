9c573631c907a39fe87d8f4bd57e6eb3e68b1354
Ahsim-Samish <s_bernaz@mail.ru>
2023-03-16 12:24:50 +0200

    zadanie 3 min.massiv

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,17 +10,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // 1 Найти максимальный элемент  и его индекс одномерного массива 
+            // 1 Найти минимальный элемент  и его индекс одномерного массива 
             int[] array = { 8, -1, 4, 16, -5, 3 };
-            int max = array[0]; int indexMax = 0;
+            int max = array[0];
+            int indexMax = 0;
+            int min = array[0];
+            int indexMin = 0;
+
+
             for (int i=1; i< array.Length; i++)
             {
-                if ( array [i] > max )
+                if ( array [i] > max)
                 {
-                   max = array[i]; indexMax = i;
+                   max = array[i];
+                    indexMax = 1;
+                }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    indexMin = i;
                 } 
             }
             Console.WriteLine($"max = {max}\t indexMax ={indexMax}");
+            Console.WriteLine($"min = {min}");
             Console.Read();
         }
     }
